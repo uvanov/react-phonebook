7422bb90c7bfd9bbe59167aae89e0a77674a73d0
uvanov <kirill.ivanov000@gmail.com>
2022-02-10 16:17:49 +0200

    Change App Styles / Import new Layout Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Navigation from "./components/Layout/Navigation/Navigation";
+import styled from 'styled-components';
+import Sidebar from "./components/Layout/Sidebar/Sidebar";
+
+
+const AppLayout = styled.div`
+        display: grid;
+        grid-template-columns: 84px 1.1fr 2fr;
+        height: 100%;
+`
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <AppLayout>
+            <Navigation/>
+            <Sidebar/>
+        </AppLayout>
     );
 };
 
