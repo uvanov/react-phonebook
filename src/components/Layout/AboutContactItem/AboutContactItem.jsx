4fc011bb6098ca906cd189c8b711c1adc8fcb137
uvanov <kirill.ivanov000@gmail.com>
2022-02-15 09:33:17 +0200

    AboutContactItem styled components

--- src/components/Layout/AboutContactItem/AboutContactItem.jsx
+++ src/components/Layout/AboutContactItem/AboutContactItem.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from "styled-components";
+
+// Styled Components
+
+const Title = styled.span`
+    font-size: 15px;
+    color: #8083A3;
+    text-transform: capitalize;
+    text-align: right;
+`
+
+const Children = styled.div`
+    font-size: 15px;
+    color: #1A1C1D;
+`
 
 const AboutContactItem = ({title, children}) => {
     return (
         <>
-            <span>{title}</span>
-            <div>{children}</div>
+            <Title>{title}</Title>
+            <Children>{children}</Children>
         </>
     );
 };
